a3c1597556fc3372220ca98179379bb19b7e3985
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-07 17:29:31 +0300

    .

--- Laboratoare/Laborator1/MyConsoleApp/Program.cs
+++ Laboratoare/Laborator1/MyConsoleApp/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+class Program
+{
+    bool Problema1(int n)
+    {
+        int inverse = 0, temp = n;
+        while (temp != 0)
+        {
+            inverse = inverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        if (n == inverse)
+            return true;
+        else
+            return false;
+    }
+
+    bool Problema2(List<int> v)
+    {
+        bool ok = true;
+        for (int i = 0; i < v.Count - 1; i++)
+        {
+            if (v[i] % 2 == v[i + 1] % 2)
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        return ok;
+            
+    }
+
+
+    static void Main(string[] args)
+    {
+        // int n = 0;
+        // String s = Console.ReadLine();
+        // if (s != null)
+        //     n = int.Parse(s); //!!!!!!
+        // if (new Program().Problema1(n))
+        //     Console.WriteLine("Numarul este palindrom");
+        // else
+        //     Console.WriteLine("Numarul nu este palindrom");
+
+        String s = Console.ReadLine();
+        List<int> v = new List<int>();
+        String[] temp = s.Split(' ');
+
+        for (int i = 0; i < temp.Length; i++)
+            v.Add(int.Parse(temp[i]));
+        
+        Program Solution = new Program();
+        if (Solution.Problema2(v))
+            Console.WriteLine("DA");
+        else
+            Console.WriteLine("NU");
+
+    }
+}
--- Laboratoare/Laborator1/MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ Laboratoare/Laborator1/MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd52675f43b1aa2589415449d4217fbb63ae593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c954ac7adfed4879489077fe3152ae17855edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
