7fa5d974b1302c714b48f4465a404a6895fea70e
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-21 16:55:46 +0300

    .

---
+++ Laboratoare/Laborator4/Models/Article.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Laborator4.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
