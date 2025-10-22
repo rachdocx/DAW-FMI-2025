3db5326581b7c3688b7542a9d9607cd82ee147c6
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-23 01:08:03 +0300

    .

--- Laboratoare/Laborator1/MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ Laboratoare/Laborator1/MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c7bcbe457d1b735dbf3f87c61940f39096b50d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa5d974b1302c714b48f4465a404a6895fea70e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Laboratoare/Laborator4/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Laborator4.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
