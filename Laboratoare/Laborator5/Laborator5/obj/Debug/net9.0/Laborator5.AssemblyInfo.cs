ae3b7dd9226937ad006c0d313862b6c9f289156d
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-28 21:29:03 +0200

    .

--- Laboratoare/Laborator5/Laborator5/obj/Debug/net9.0/Laborator5.AssemblyInfo.cs
+++ Laboratoare/Laborator5/Laborator5/obj/Debug/net9.0/Laborator5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laborator5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3b1ce236821503b2ca0d7a13d80c6b97476c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b44e6b8fc511db6def2a4c31297f02b2f4c12f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laborator5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laborator5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
